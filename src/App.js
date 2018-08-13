2c18d93f622e7177a1e54e474c53fbb53279fc6f
BrandonEscamilla <brandon.escamilla@outlook.com>
2018-08-13 14:25:17 -0500

    PWA changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intr">
+        <p className="App-intro">
             Este es para el joaqo
         </p>
       </div>
