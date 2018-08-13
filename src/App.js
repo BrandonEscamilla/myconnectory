26950664b7d2d6a12ffe455a3827210c1bcd9bd4
BrandonEscamilla <brandon.escamilla@outlook.com>
2018-08-13 14:03:38 -0500

    Test error

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intr">
             Este es para el joaqo
         </p>
       </div>
